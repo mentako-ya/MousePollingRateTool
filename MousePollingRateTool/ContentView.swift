8222ada796c6e6202c97093f3fa2aef665c60598
mentako-ya <mentakoya@gmail.com>
2026-03-20 20:10:49 +0900

    Initial Commit

---
+++ MousePollingRateTool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MousePollingRateTool
+//
+//  Created by tools on 2026/03/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MousePollingRateTool/MousePollingRateToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  MousePollingRateToolApp.swift
+//  MousePollingRateTool
+//
+//  Created by tools on 2026/03/20.
+//
+
+import SwiftUI
+
+@main
+struct MousePollingRateToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
